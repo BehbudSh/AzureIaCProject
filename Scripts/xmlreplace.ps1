08d9d480447d2c0ad737f3da3f556e4fc215b126
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-08-14 18:47:17 +0400

    adding output to json

--- Scripts/xmlreplace.ps1
+++ Scripts/xmlreplace.ps1
@@ -1,7 +1,7 @@
 param(
-    [string]$PathToXml
+    [string]$PathToXml,
+    [string]$DatabaseConnectionString
 )
-$xmlreplace = (Get-AzureRmSqlDatabaseSecureConnectionPolicy -ResourceGroupName "Project1RG" -ServerName "prodvmsql" -DatabaseName "sqldb").ConnectionStrings.AdoNetConnectionString
 $xml = [xml](Get-Content -Path $PathToXml)
-$xml.configuration.connectionStrings.ChildNodes.Item(0).connectionstring = $xmlreplace
+$xml.configuration.connectionStrings.ChildNodes.Item(0).connectionstring = $DatabaseConnectionString
 $xml.Save($PathToXml)
