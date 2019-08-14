9a721ccbe7101ec8f7cac116f7f0a9aba18936a0
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-08-14 18:14:52 +0400

    adding Parameter PathtoXml

--- Scripts/xmlreplace.ps1
+++ Scripts/xmlreplace.ps1
@@ -1,6 +1,7 @@
+param(
+    [string]$PathToXml
+)
 $xmlreplace = (Get-AzureRmSqlDatabaseSecureConnectionPolicy -ResourceGroupName "Project1RG" -ServerName "prodvmsql" -DatabaseName "sqldb").ConnectionStrings.AdoNetConnectionString
-$PathToXml =  '$(Build.ArtifactStagingDirectory)/drop.be/Web.config'
 $xml = [xml](Get-Content -Path $PathToXml)
 $xml.configuration.connectionStrings.ChildNodes.Item(0).connectionstring = $xmlreplace
 $xml.Save($PathToXml)
-
